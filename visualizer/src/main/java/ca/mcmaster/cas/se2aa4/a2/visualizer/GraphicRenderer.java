3f63f302a5e8c8b6ef3f75e20d8a95e1fedde8c6
Jianqing Liu <liu927@mcmaster.ca>
2023-02-08 16:09:14 -0500

    1b renderer

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.cas.se2aa4.a2.visualizer;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
@@ -9,31 +10,40 @@ import java.awt.Stroke;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.util.List;
 
 public class GraphicRenderer {
 
     private static final int THICKNESS = 3;
-    public void render(Mesh aMesh, Graphics2D canvas) {
+
+    public void render(Mesh mesh, Graphics2D canvas) {
         canvas.setColor(Color.BLACK);
         Stroke stroke = new BasicStroke(0.5f);
         canvas.setStroke(stroke);
-        for (Vertex v: aMesh.getVerticesList()) {
-            double centre_x = v.getX() - (THICKNESS/2.0d);
-            double centre_y = v.getY() - (THICKNESS/2.0d);
-            Color old = canvas.getColor();
+        List<Vertex> vertices = mesh.getVerticesList();
+        for (Vertex v : vertices) {
+            double centre_x = v.getX() - (THICKNESS / 2.0d);
+            double centre_y = v.getY() - (THICKNESS / 2.0d);
             canvas.setColor(extractColor(v.getPropertiesList()));
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
-            canvas.setColor(old);
+        }
+        for (Segment segment : mesh.getSegmentsList()) {
+            System.out.println(segment);
+            Vertex a = vertices.get(segment.getV1Idx());
+            Vertex b = vertices.get(segment.getV2Idx());
+            canvas.setColor(extractColor(segment.getPropertiesList()));
+            Line2D line = new Line2D.Double(a.getX(), a.getY(), b.getX(), b.getY());
+            canvas.draw(line);
         }
     }
 
     private Color extractColor(List<Property> properties) {
         String val = null;
-        for(Property p: properties) {
+        for (Property p : properties) {
             if (p.getKey().equals("rgb_color")) {
-                System.out.println(p.getValue());
+                // System.out.println(p.getValue());
                 val = p.getValue();
             }
         }
