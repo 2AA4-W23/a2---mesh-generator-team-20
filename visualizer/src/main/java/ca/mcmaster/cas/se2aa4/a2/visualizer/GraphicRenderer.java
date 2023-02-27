429f8e11571dd4981071d1ecccbf932bbe5bdcce
Jianqing Liu <liu927@mcmaster.ca>
2023-02-27 18:12:46 -0500

    add logs for visualizer

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -20,37 +20,44 @@ public class GraphicRenderer {
     private static final Logger logger = LogManager.getLogger(GraphicRenderer.class);
 
     public void render(Mesh mesh, Graphics2D canvas) {
-        canvas.setColor(Color.BLACK);
-        Stroke stroke = new BasicStroke(0.5f);
-        canvas.setStroke(stroke);
+        boolean isDebug = System.getProperty("env").equals("debug");
+        if (isDebug) {
+            logger.info("Debug mode detected");
+        }
+
         List<Vertex> vertices = mesh.getVerticesList();
+        // draw all vertices
         for (Vertex v : vertices) {
+            logger.trace("drawing vertex: {}", v);
             double centre_x = v.getX() - (extractThickness(v.getPropertiesList()) / 2.0d);
             double centre_y = v.getY() - (extractThickness(v.getPropertiesList()) / 2.0d);
 
-            if(System.getProperty("env").equals("debug") && extractCentroid(v.getPropertiesList())){
-                canvas.setColor(new Color(255,0,0));
-            }else{
+            // set centroid color to red if in debug mode
+            if (isDebug && extractIsCentroid(v.getPropertiesList())) {
+                canvas.setColor(new Color(255, 0, 0));
+            } else {
                 canvas.setColor(extractColor(v.getPropertiesList()));
             }
 
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, extractThickness(v.getPropertiesList()), extractThickness(v.getPropertiesList()));
             canvas.fill(point);
         }
+
+        // draw all segments
         for (Segment segment : mesh.getSegmentsList()) {
-            logger.info(segment);
+            logger.trace("drawing segment: {}", segment);
             Vertex a = vertices.get(segment.getV1Idx());
             Vertex b = vertices.get(segment.getV2Idx());
-            if(System.getProperty("env").equals("debug")){
-                // Debugmode: function color (0,0,0)
-                canvas.setColor(new Color(0,0,0));
-            }else{
+
+            // set segment color to black if in debug mode
+            if (isDebug) {
+                canvas.setColor(new Color(0, 0, 0));
+            } else {
                 canvas.setColor(extractColor(segment.getPropertiesList()));
             }
 
-
             // thickness
-            stroke = new BasicStroke(extractThickness(segment.getPropertiesList()));
+            Stroke stroke = new BasicStroke(extractThickness(segment.getPropertiesList()));
             canvas.setStroke(stroke);
             Line2D line = new Line2D.Double(a.getX(), a.getY(), b.getX(), b.getY());
             canvas.draw(line);
@@ -58,13 +65,7 @@ public class GraphicRenderer {
     }
 
     private Color extractColor(List<Property> properties) {
-        String val = null;
-        for (Property p : properties) {
-            if (p.getKey().equals("rgb_color")) {
-                logger.info(p.getValue());
-                val = p.getValue();
-            }
-        }
+        String val = getProperty(properties, "rgb_color");
         if (val == null)
             return Color.BLACK;
         String[] raw = val.split(",");
@@ -75,32 +76,19 @@ public class GraphicRenderer {
     }
 
     // extract thickness
-    //prop [color: [red,blue,yellow]
+    // prop [color: [red,blue,yellow]
     //     [Thickness: [int]]
     private int extractThickness(List<Property> properties) {
-        String val = null;
-        for (Property p : properties) {
-            if (p.getKey().equals("thickness")) {
-                // System.out.println(p.getValue());
-                val = p.getValue();
-            }
-        }
+        String val = getProperty(properties, "thickness");
         if (val == null) {
             return 1;
         }
-        int raw = Integer.parseInt(val);
-        return raw;
+        return Integer.parseInt(val);
     }
 
     // extract centroid; [True]
-    private boolean extractCentroid(List<Property> properties){
-        String val = null;
-        for (Property p : properties) {
-            if (p.getKey().equals("centroid")) {
-                // System.out.println(p.getValue());
-                val = p.getValue();
-            }
-        }
+    private boolean extractIsCentroid(List<Property> properties) {
+        String val = getProperty(properties, "centroid");
         if (val == null) {
             return false;
         }
@@ -108,5 +96,12 @@ public class GraphicRenderer {
         return val.equals("true");
     }
 
-
+    private String getProperty(List<Property> properties, String key) {
+        for (Property p : properties) {
+            if (p.getKey().equals(key)) {
+                return p.getValue();
+            }
+        }
+        return null;
+    }
 }
