a282c00cb26811474934caa295e19434c8025935
Jianqing Liu <liu927@mcmaster.ca>
2023-03-23 16:43:57 -0400

    add alpha to color

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/color/Color.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/color/Color.java
@@ -1,13 +1,25 @@
 package ca.mcmaster.cas.se2aa4.a3.island.color;
 
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
 public class Color {
     final double red;
     final double green;
     final double blue;
+    public int alpha;
 
-    public Color(double red, double green, double blue) {
+    public Color(double red, double green, double blue, int alpha) {
         this.red = red;
         this.green = green;
         this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    public String toString() {
+        return red + "," + green + "," + blue + "," + alpha;
+    }
+
+    public Structs.Property toProperty() {
+        return Structs.Property.newBuilder().setKey("rgb_color").setValue(toString()).build();
     }
 }
