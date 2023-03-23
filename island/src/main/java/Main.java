5140fa89b25525843caaf50e222952d3bb42fad8
grace <gracegracexiao@gmail.com>
2023-03-23 16:23:26 -0400

    1 CLI for island

--- island/src/main/java/Main.java
+++ island/src/main/java/Main.java
@@ -1,11 +1,45 @@
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import org.apache.commons.cli.Option;
 
 import java.awt.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import org.apache.commons.cli.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static final Option INPUTMESH = new Option("i", "inputmesh", false, "create a grid mesh");
+    private static final Option LAGOONMESH = new Option("o", "lagoonmesh", false, "create an irregular mesh");
+    private static final Option LAGOON = new Option("m", "lagoon", false, "create an triangle mesh");
 
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        PrintWriter pw = new PrintWriter(System.out);
+        formatter.printUsage(pw, 100, "java -jar island.jar -i input.mesh -o lagoon.mesh --mode lagoon");
+        formatter.printOptions(pw, 100, options, 2, 5);
+        pw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        //java -jar island.jar -i input.mesh -o lagoon.mesh --mode lagoon
+        Options options = new Options();
+        options.addOption(INPUTMESH);
+        options.addOption(LAGOONMESH);
+        options.addOption(LAGOON);
+
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            CommandLine cmd = parser.parse(options, args);
+
+            if (cmd.getArgList().size() < 1) {
+                printHelp(options);
+                System.exit(-1);
+            }
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
