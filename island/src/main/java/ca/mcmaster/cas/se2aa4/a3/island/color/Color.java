cb7804529bacbfdf5516bceeed28278c4a688f3c
Jianqing Liu <liu927@mcmaster.ca>
2023-03-23 16:23:41 -0400

    color provider

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/color/Color.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas.se2aa4.a3.island.color;
+
+public class Color {
+    final double red;
+    final double green;
+    final double blue;
+
+    public Color(double red, double green, double blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
