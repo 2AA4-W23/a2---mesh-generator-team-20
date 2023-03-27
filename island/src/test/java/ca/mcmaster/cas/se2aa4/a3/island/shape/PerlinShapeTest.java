357545604e530b5f51ec156ad99d23755fe113f4
grace <gracegracexiao@gmail.com>
2023-03-26 23:46:11 -0400

    PerlinShapeTest

--- island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/shape/PerlinShapeTest.java
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/shape/PerlinShapeTest.java
@@ -1,4 +1,20 @@
 package ca.mcmaster.cas.se2aa4.a3.island.shape;
 
+import ca.mcmaster.cas.se2aa4.a3.island.utils.Coordinate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PerlinShapeTest {
+
+    @Test
+    void testIsLand() {
+        PerlinShape perlinShape = new PerlinShape(1234L);
+
+        // Test some coordinates that are expected to be land and water
+        assertEquals(perlinShape.isLand(new Coordinate(0, 0)), false);
+        assertEquals(perlinShape.isLand(new Coordinate(0, 100)), true);
+        assertEquals(perlinShape.isLand(new Coordinate(100, 0)), true);
+        assertEquals(perlinShape.isLand(new Coordinate(100, 100)), true);
+    }
 }
