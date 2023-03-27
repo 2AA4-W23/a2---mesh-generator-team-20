81ca284b0b46963c916c6772b62e12b325390a9c
grace <gracegracexiao@gmail.com>
2023-03-26 23:53:08 -0400

    BasicSoilAbsorptionProviderTest

--- island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/soil/BasicSoilAbsorptionProviderTest.java
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/soil/BasicSoilAbsorptionProviderTest.java
@@ -1,4 +1,21 @@
 package ca.mcmaster.cas.se2aa4.a3.island.soil;
 
-public class BasicSoilAbsorptionProviderTest {
+import ca.mcmaster.cas.se2aa4.a3.island.aquifer.AquiferProvider;
+import ca.mcmaster.cas.se2aa4.a3.island.lake.CircleLakeProvider;
+import ca.mcmaster.cas.se2aa4.a3.island.lake.LakeProvider;
+import ca.mcmaster.cas.se2aa4.a3.island.river.RiverProvider;
+import ca.mcmaster.cas.se2aa4.a3.island.shape.CircleShape;
+import ca.mcmaster.cas.se2aa4.a3.island.shape.ShapeProvider;
+import ca.mcmaster.cas.se2aa4.a3.island.utils.Coordinate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BasicSoilAbsorptionProviderTest {
+
+    @Test
+    void testGetAbsorptionLevel() {
+
+    }
+
 }
